9d3e5aee27db20ccce963e85e6698a2fc2165acf
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-08-25 19:32:13 +0200

    init

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+
+class Document extends NextDocument {
+    public render(): JSX.Element {
+        return (
+            <Html lang="cs">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default Document;
