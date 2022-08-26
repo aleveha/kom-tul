59a75137646899ac93bdc3bd6b43708740f2a109
Nikita Tashchilin <nikita.tashilin@gmail.com>
2022-08-26 17:09:51 +0200

    add fonts and base styling

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,5 +1,15 @@
 import React, { FC } from "react";
 
 export const Page: FC = () => {
-	return <h1>KOM TUL WEB</h1>;
+	return (
+		<div>
+			<h1 className="ui-typo-hero">Just a text</h1>
+			<h1 className="ui-typo-h1">Just a text</h1>
+			<h2 className="ui-typo-h2">Just a text</h2>
+			<h3 className="ui-typo-h3">Just a text</h3>
+			<p className="ui-typo-large">Just a text</p>
+			<p className="ui-typo-body">Just a text</p>
+			<p className="ui-typo-small">Just a text</p>
+		</div>
+	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
-/** @type {import('tailwindcss').Config} */
+const em = value => value / 16 + "em";
+const rem = value => value / 16 + "rem";
+
+/** @type {import("tailwindcss").Config} */
 module.exports = {
 	content: ["./src/**/*.{ts,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			"tul-mono":
+				"tul-mono-custom, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+			base: "inter-custom, system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+		},
+		screens: {
+			sm: em(576),
+			md: em(768),
+			lg: em(992),
+			xl: em(1280),
+			xxl: em(1440),
+		},
+		container: {
+			sm: "none",
+			md: rem(688),
+			lg: rem(912),
+			xl: rem(1120),
+			xxl: rem(1216),
+		},
+		colors: {
+			tulViolet: "#5948AD",
+			tulVioletDark: "#251E48",
+			gray500: "#888B95",
+			gray200: "#C9CACF",
+			gray100: "#E4E5E7",
+			white: "#FFFFFF",
+		},
+		fontSize: {
+			hero: [rem(72)],
+			h1: [rem(32)],
+			h2: [rem(28)],
+			h3: [rem(24)],
+			large: [rem(20)],
+			body: [rem(18)],
+			small: [rem(12)],
+		},
 	},
 	plugins: [],
 };
